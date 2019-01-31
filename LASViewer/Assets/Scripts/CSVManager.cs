42c9f4d4798112d274c8b979538251c1f5985e18
amazingsmash <amazingsmash@gmail.com>
2019-01-31 19:07:22 +0000

    init

---
+++ LASViewer/Assets/Scripts/CSVManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVManager {
+
+    public static float[,] ReadFloatCSVMatrix(TextAsset csvFile)
+    {
+        return ReadFloatCSVMatrix(csvFile.text);
+    }
+
+    public static float[,] ReadFloatCSVMatrix(string csvString)
+    {
+        string[] lines = csvString.Split("\n"[0]);
+        int nRows = lines.Length;
+
+        if (lines[lines.Length - 1].Length == 0)
+        {
+            nRows--;
+        }
+
+
+        int nCol = lines[0].Split(',').Length;
+        float[,] values = new float[nRows, nCol];
+        for (int i = 0; i < nRows; i++)
+        {
+            string l = lines[i];
+            string[] numStr = l.Split(',');
+
+            if (numStr.Length == nCol) { 
+                for (int j = 0; j < nCol; j++)
+                {
+                    try { values[i, j] = float.Parse(numStr[j]); }
+                    catch (System.FormatException e)
+                    {
+                        Debug.Log("Problem parsing " + numStr[j]);
+                    }
+
+                }
+            }
+        }
+
+        return values;
+    }
+
+    //static private string[] SplitCsvLine(string line)
+    //{
+    //    return (from System.Text.RegularExpressions.Match m in System.Text.RegularExpressions.Regex.Matches(line,
+    //        @"(((?<x>(?=[,\r\n]+))|""(?<x>([^""]|"""")+)""|(?<x>[^,\r\n]+)),?)",
+    //        System.Text.RegularExpressions.RegexOptions.ExplicitCapture)
+    //            select m.Groups[1].Value).ToArray();
+    //}
+
+
+}
---
+++ LASViewer/Assets/Scripts/Matrix2D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matrix2D {
+
+    public readonly float[,] values = null;
+
+    public Matrix2D(float[,] values){
+        this.values = values;
+    }
+
+    private static float[] bytes2Float(byte[] bytes){
+
+        float[] fs = new float[bytes.Length / 4];
+        for (int i = 0, j = 0; i < bytes.Length; i+=4, j++)
+        {
+            fs[j] = System.BitConverter.ToSingle(bytes, i);
+        }
+        return fs;
+
+    }
+
+    public static Matrix2D readFromBytes(byte[] bytes){
+
+        float[] buffer = bytes2Float(bytes);
+        int nRows = (int)buffer[0];
+        int nCols = (int)buffer[1];
+        float[,] values = new float[(int)buffer[0], (int)buffer[1]];
+
+        int offset = 2;
+
+        for (int j = 0; j < nCols; j++){
+            for (int i = 0; i < nRows; i++){
+                values[i, j] = buffer[offset];
+                offset+=1;
+            }
+        }
+
+        //Debug.Log(values);
+
+        return new Matrix2D(values);
+    }
+}

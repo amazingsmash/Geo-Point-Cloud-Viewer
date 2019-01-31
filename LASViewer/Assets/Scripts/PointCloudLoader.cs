6c80c0ff0306bc66801429225359b64cdf65f95b
amazingsmash <amazingsmash@gmail.com>
2019-01-31 20:08:21 +0000

    PC Part

--- LASViewer/Assets/Scripts/PointCloudLoader.cs
+++ LASViewer/Assets/Scripts/PointCloudLoader.cs
@@ -9,72 +9,73 @@ using System.IO;
 public class PointCloudLoader : MonoBehaviour
 {
 
-    public string LASByteFolderName = "7";
+    //public string LASByteFolderName = "7";
 
-    public static string getAssetsPath()
-    {
-        string assetsPath = "";
-        RuntimePlatform platform = Application.platform;
-        switch (platform)
-        {
-            case RuntimePlatform.Android:
-                assetsPath = "jar:file://" + Application.dataPath + "!/assets";
-                break;
-            case RuntimePlatform.IPhonePlayer:
-                assetsPath = Application.dataPath + "/Raw";
-                break;
-            case RuntimePlatform.OSXEditor:
-            case RuntimePlatform.WindowsEditor:
-            case RuntimePlatform.WindowsPlayer:
-                assetsPath = Application.dataPath + "/StreamingAssets";
-                break;
-            case RuntimePlatform.OSXPlayer:
-                assetsPath = Application.dataPath + "/Resources/Data/StreamingAssets";
-                break;
-            default:
-                Debug.Log("Unrecognized platform.");
-                break;
-        }
+    //public static string getAssetsPath()
+    //{
+    //    string assetsPath = "";
+    //    RuntimePlatform platform = Application.platform;
+    //    switch (platform)
+    //    {
+    //        case RuntimePlatform.Android:
+    //            assetsPath = "jar:file://" + Application.dataPath + "!/assets";
+    //            break;
+    //        case RuntimePlatform.IPhonePlayer:
+    //            assetsPath = Application.dataPath + "/Raw";
+    //            break;
+    //        case RuntimePlatform.OSXEditor:
+    //        case RuntimePlatform.WindowsEditor:
+    //        case RuntimePlatform.WindowsPlayer:
+    //            assetsPath = Application.dataPath + "/StreamingAssets";
+    //            break;
+    //        case RuntimePlatform.OSXPlayer:
+    //            assetsPath = Application.dataPath + "/Resources/Data/StreamingAssets";
+    //            break;
+    //        default:
+    //            Debug.Log("Unrecognized platform.");
+    //            break;
+    //    }
 
-        return assetsPath;
-    }
+    //    return assetsPath;
+    //}
 
-    void createChildWithMesh(Mesh mesh){
-        GameObject child = new GameObject("PointCloud");
-        child.AddComponent<MeshFilter>();
-        child.AddComponent<MeshRenderer>();
-        child.GetComponent<MeshFilter>().mesh = mesh;
-        child.GetComponent<MeshRenderer>().material = GetComponent<MeshRenderer>().material;
-        child.transform.SetParent(this.transform, false);
+    //void createChildWithMesh(Mesh mesh){
+    //    GameObject child = new GameObject("PointCloud");
+    //    child.AddComponent<MeshFilter>();
+    //    child.AddComponent<MeshRenderer>();
+    //    child.GetComponent<MeshFilter>().mesh = mesh;
+    //    child.GetComponent<MeshRenderer>().material = GetComponent<MeshRenderer>().material;
+    //    child.transform.SetParent(this.transform, false);
 
-        child.AddComponent<BoxCollider>();
-        Bounds b = child.GetComponent<MeshFilter>().mesh.bounds;
-        child.GetComponent<BoxCollider>().center = b.center;
-        child.GetComponent<BoxCollider>().size = b.size;
-    }
+    //    child.AddComponent<BoxCollider>();
+    //    Bounds b = child.GetComponent<MeshFilter>().mesh.bounds;
+    //    child.GetComponent<BoxCollider>().center = b.center;
+    //    child.GetComponent<BoxCollider>().size = b.size;
+    //}
 
-    void readFromCSV(){
+    //void readFromCSV(){
 
-        DirectoryInfo dir = new DirectoryInfo(getAssetsPath() + "/LAS_CSV_2");
-        FileInfo[] info = dir.GetFiles("*.*");
-        foreach (FileInfo f in info)
-        {
-            if (f.FullName.EndsWith("txt"))
-            {
-                StreamReader reader = new StreamReader(f.FullName);
-                string csv = reader.ReadToEnd();
+    //    DirectoryInfo dir = new DirectoryInfo(getAssetsPath() + "/LAS_CSV_2");
+    //    FileInfo[] info = dir.GetFiles("*.*");
+    //    foreach (FileInfo f in info)
+    //    {
+    //        if (f.FullName.EndsWith("txt"))
+    //        {
+    //            StreamReader reader = new StreamReader(f.FullName);
+    //            string csv = reader.ReadToEnd();
 
-                float[,] mat = CSVManager.ReadFloatCSVMatrix(csv);
-                Debug.Log("Read matrix " + mat.GetLength(0) + " x " + mat.GetLength(1));
-                //mesh = createMeshFromLASMatrix(mat);
-                Mesh mesh = createMeshFromLASMatrix(mat);
-                createChildWithMesh(mesh);
+    //            float[,] mat = CSVManager.ReadFloatCSVMatrix(csv);
+    //            Debug.Log("Read matrix " + mat.GetLength(0) + " x " + mat.GetLength(1));
+    //            //mesh = createMeshFromLASMatrix(mat);
+    //            Mesh mesh = createMeshFromLASMatrix(mat);
+    //            createChildWithMesh(mesh);
 
-                //break; //Just 1 cloud
-            }
-        }
 
-    }
+    //            //break; //Just 1 cloud
+    //        }
+    //    }
+
+    //}
 
     DirectoryInfo getModelDirectory(){
         string path = EditorUtility.OpenFolderPanel("Select Model Folder", "", "");
@@ -93,84 +94,146 @@ public class PointCloudLoader : MonoBehaviour
             FileInfo[] info = dir.GetFiles("*.bytes");
             foreach (FileInfo f in info)
             {
-                byte[] buffer = File.ReadAllBytes(f.FullName);
-                Matrix2D m = Matrix2D.readFromBytes(buffer);
-                Mesh mesh = createMeshFromLASMatrix(m.values);
-                createChildWithMesh(mesh);
+                //byte[] buffer = File.ReadAllBytes(f.FullName);
+                //Matrix2D m = Matrix2D.readFromBytes(buffer);
+                //Mesh mesh = createMeshFromLASMatrix(m.values);
+                //createChildWithMesh(mesh);
+
+
+                GameObject child = new GameObject("PointCloud");
+                child.AddComponent<PointCloudPart>();
+                child.GetComponent<PointCloudPart>().initWithFilePath(f.FullName, GetComponent<MeshRenderer>().material);
+                child.transform.SetParent(this.transform, false);
             }
         }
     }
 
-    Dictionary<float, Color> classColor = null;
-    Color getColorForClass(float classification){
+    //Dictionary<float, Color> classColor = null;
+    //Color getColorForClass(float classification){
 
-        if (classColor == null)
-        {
-            classColor = new Dictionary<float, Color>();
-            classColor[16] = Color.blue;
-            classColor[19] = Color.blue;
-            classColor[17] = Color.red;
-            classColor[20] = Color.green;
-            classColor[31] = new Color(244.0f / 255.0f, 191.0f / 255.0f, 66.0f / 255.0f);
-            classColor[29] = Color.black;
-            classColor[30] = new Color(244.0f / 255.0f, 65.0f / 255.0f, 244.0f / 255.0f);
-        }
+    //    if (classColor == null)
+    //    {
+    //        classColor = new Dictionary<float, Color>();
+    //        classColor[16] = Color.blue;
+    //        classColor[19] = Color.blue;
+    //        classColor[17] = Color.red;
+    //        classColor[20] = Color.green;
+    //        classColor[31] = new Color(244.0f / 255.0f, 191.0f / 255.0f, 66.0f / 255.0f);
+    //        classColor[29] = Color.black;
+    //        classColor[30] = new Color(244.0f / 255.0f, 65.0f / 255.0f, 244.0f / 255.0f);
+    //    }
 
-        if (classColor.ContainsKey(classification)){
-            return classColor[classification];
-        } else{
-            return Color.gray;
-        }
+    //    if (classColor.ContainsKey(classification)){
+    //        return classColor[classification];
+    //    } else{
+    //        return Color.gray;
+    //    }
+    //}
 
-    }
+    //Mesh createMeshFromLASMatrix(float[,] matrix)
+    //{
+    //    int nPoints = matrix.GetLength(0);
+    //    Mesh mesh = new Mesh();
+    //    Vector3[] points = new Vector3[nPoints];
+    //    int[] indices = new int[nPoints];
+    //    Color[] colors = new Color[nPoints];
 
-    Mesh createMeshFromLASMatrix(float[,] matrix)
-    {
-        int nPoints = matrix.GetLength(0);
-        Mesh mesh = new Mesh();
-        Vector3[] points = new Vector3[nPoints];
-        int[] indices = new int[nPoints];
-        Color[] colors = new Color[nPoints];
+    //    for (int i = 0; i < nPoints; i++)
+    //    {
+    //        points[i] = new Vector3(matrix[i, 0], matrix[i, 1], matrix[i, 2]);
+    //        indices[i] = i;
+    //        float classification = matrix[i, 3];
+    //        colors[i] = getColorForClass(classification);
+    //    }
 
-        for (int i = 0; i < nPoints; i++)
-        {
-            points[i] = new Vector3(matrix[i, 0], matrix[i, 1], matrix[i, 2]);
-            indices[i] = i;
-            float classification = matrix[i, 3];
-            colors[i] = getColorForClass(classification);
-        }
+    //    mesh.vertices = points;
+    //    mesh.colors = colors;
+    //    mesh.SetIndices(indices, MeshTopology.Points, 0);
 
-        mesh.vertices = points;
-        mesh.colors = colors;
-        mesh.SetIndices(indices, MeshTopology.Points, 0);
+    //    Debug.Log("Generated Point Cloud Mesh with " + nPoints + " points.");
 
-        Debug.Log("Generated Point Cloud Mesh with " + nPoints + " points.");
+    //    return mesh;
+    //}
 
-        return mesh;
-    }
+    //Mesh createRandomCubeMesh()
+    //{
+    //    int numPoints = 60000;
 
-    Mesh createRandomCubeMesh()
-    {
-        int numPoints = 60000;
+    //    Mesh mesh = new Mesh();
+    //    Vector3[] points = new Vector3[numPoints];
+    //    int[] indices = new int[numPoints];
+    //    Color[] colors = new Color[numPoints];
+    //    for (int i = 0; i < points.Length; ++i)
+    //    {
+    //        points[i] = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+    //        indices[i] = i;
+    //        colors[i] = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+    //    }
 
-        Mesh mesh = new Mesh();
-        Vector3[] points = new Vector3[numPoints];
-        int[] indices = new int[numPoints];
-        Color[] colors = new Color[numPoints];
-        for (int i = 0; i < points.Length; ++i)
-        {
-            points[i] = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
-            indices[i] = i;
-            colors[i] = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
-        }
-
-        mesh.vertices = points;
-        mesh.colors = colors;
-        mesh.SetIndices(indices, MeshTopology.Points, 0);
-        return mesh;
-    }
+    //    mesh.vertices = points;
+    //    mesh.colors = colors;
+    //    mesh.SetIndices(indices, MeshTopology.Points, 0);
+    //    return mesh;
+    //}
 
     void Update()
+    {
+        selectPoint();
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Debug.Log("Finding selected point.");
+
+        //    Vector3 mPos = Input.mousePosition;
+        //    MeshFilter[] mf = GetComponentsInChildren<MeshFilter>();
+        //    const float maxDist = 10000000.0f;
+
+        //    Vector3 closestHit = Vector3.negativeInfinity;
+        //    float minHitDistance = maxDist;
+
+        //    foreach (Transform child in transform){
+
+
+        //        Collider coll = child.GetComponent<Collider>();
+        //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //        RaycastHit hit;
+        //        if (!coll.bounds.Contains(ray.origin) && !coll.Raycast(ray, out hit, maxDist))
+        //        {
+        //            continue;
+        //        }
+
+        //        Mesh mesh = child.GetComponent<MeshFilter>().mesh;
+
+
+        //        print("Scanning Point Cloud with " + mesh.vertices.Length + " vertices.");
+        //        foreach (Vector3 p in mesh.vertices)
+        //        {
+        //            Vector3 pWorld = transform.TransformPoint(p);
+        //            Vector3 v = Camera.main.WorldToScreenPoint(pWorld);
+        //            float distancePointToCamera = Mathf.Abs(v.z);
+        //            if (distancePointToCamera < minHitDistance)
+        //            {
+        //                float sqrDistance = (new Vector2(v.x, v.y) - new Vector2(mPos.x, mPos.y)).sqrMagnitude;
+        //                if (sqrDistance < 25.0f)
+        //                {
+        //                    closestHit = pWorld;
+        //                    minHitDistance = distancePointToCamera;
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    if (!closestHit.Equals(Vector3.negativeInfinity))
+        //    {
+        //        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //        sphere.transform.position = closestHit;
+        //        sphere.transform.localScale = new Vector3(4.0f, 4.0f, 4.0f);
+        //    }
+
+        //}
+    }
+
+
+    void selectPoint()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -178,41 +241,14 @@ public class PointCloudLoader : MonoBehaviour
 
             Vector3 mPos = Input.mousePosition;
             MeshFilter[] mf = GetComponentsInChildren<MeshFilter>();
-            const float maxDist = 10000000.0f;
+            float maxDist = 10000000.0f;
 
             Vector3 closestHit = Vector3.negativeInfinity;
-            float minHitDistance = maxDist;
 
-            foreach (Transform child in transform){
-
-
-                Collider coll = child.GetComponent<Collider>();
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (!coll.bounds.Contains(ray.origin) && !coll.Raycast(ray, out hit, maxDist))
-                {
-                    continue;
-                }
-
-                Mesh mesh = child.GetComponent<MeshFilter>().mesh;
-
-
-                print("Scanning Point Cloud with " + mesh.vertices.Length + " vertices.");
-                foreach (Vector3 p in mesh.vertices)
-                {
-                    Vector3 pWorld = transform.TransformPoint(p);
-                    Vector3 v = Camera.main.WorldToScreenPoint(pWorld);
-                    float distancePointToCamera = Mathf.Abs(v.z);
-                    if (distancePointToCamera < minHitDistance)
-                    {
-                        float sqrDistance = (new Vector2(v.x, v.y) - new Vector2(mPos.x, mPos.y)).sqrMagnitude;
-                        if (sqrDistance < 25.0f)
-                        {
-                            closestHit = pWorld;
-                            minHitDistance = distancePointToCamera;
-                        }
-                    }
-                }
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            foreach (Transform child in transform)
+            {
+                child.GetComponent<PointCloudPart>().getClosestPointOnRay(ray, mPos, ref maxDist, ref closestHit);
             }
 
             if (!closestHit.Equals(Vector3.negativeInfinity))
---
+++ LASViewer/Assets/Scripts/PointCloudPart.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+
+public class PointCloudPart : MonoBehaviour {
+
+    Mesh mesh = null;
+    string filePath = null;
+    Material material = null;
+
+    public void initWithFilePath(string filePath, Material material){
+        this.filePath = filePath;
+        this.material = material;
+        byte[] buffer = File.ReadAllBytes(filePath);
+        Matrix2D m = Matrix2D.readFromBytes(buffer);
+        mesh = createMeshFromLASMatrix(m.values);
+    }
+
+    // Use this for initialization
+    void Start () {
+        gameObject.name = (new FileInfo(filePath)).Name;
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        gameObject.GetComponent<MeshRenderer>().material = material;
+
+        gameObject.AddComponent<BoxCollider>();
+        Bounds b = gameObject.GetComponent<MeshFilter>().mesh.bounds;
+        gameObject.GetComponent<BoxCollider>().center = b.center;
+        gameObject.GetComponent<BoxCollider>().size = b.size;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //-------------
+
+    Dictionary<float, Color> classColor = null;
+    Color getColorForClass(float classification)
+    {
+
+        if (classColor == null)
+        {
+            classColor = new Dictionary<float, Color>();
+            classColor[16] = Color.blue;
+            classColor[19] = Color.blue;
+            classColor[17] = Color.red;
+            classColor[20] = Color.green;
+            classColor[31] = new Color(244.0f / 255.0f, 191.0f / 255.0f, 66.0f / 255.0f);
+            classColor[29] = Color.black;
+            classColor[30] = new Color(244.0f / 255.0f, 65.0f / 255.0f, 244.0f / 255.0f);
+        }
+
+        if (classColor.ContainsKey(classification))
+        {
+            return classColor[classification];
+        }
+        else
+        {
+            return Color.gray;
+        }
+    }
+
+    Mesh createMeshFromLASMatrix(float[,] matrix)
+    {
+        int nPoints = matrix.GetLength(0);
+        Mesh pointCloud = new Mesh();
+        Vector3[] points = new Vector3[nPoints];
+        int[] indices = new int[nPoints];
+        Color[] colors = new Color[nPoints];
+
+        for (int i = 0; i < nPoints; i++)
+        {
+            points[i] = new Vector3(matrix[i, 0], matrix[i, 1], matrix[i, 2]);
+            indices[i] = i;
+            float classification = matrix[i, 3];
+            colors[i] = getColorForClass(classification);
+        }
+
+        pointCloud.vertices = points;
+        pointCloud.colors = colors;
+        pointCloud.SetIndices(indices, MeshTopology.Points, 0);
+
+        Debug.Log("Generated Point Cloud Mesh with " + nPoints + " points.");
+
+        return pointCloud;
+    }
+
+    public void getClosestPointOnRay(Ray ray, Vector2 screenPos, ref float maxDist, ref Vector3 closestHit)
+    {
+        Collider coll = gameObject.GetComponent<Collider>();
+        RaycastHit hit;
+        if (coll.bounds.Contains(ray.origin) || coll.Raycast(ray, out hit, maxDist))
+        {
+            print("Scanning Point Cloud with " + mesh.vertices.Length + " vertices.");
+            foreach (Vector3 p in mesh.vertices)
+            {
+                Vector3 pWorld = transform.TransformPoint(p);
+                Vector3 v = Camera.main.WorldToScreenPoint(pWorld);
+                float distancePointToCamera = Mathf.Abs(v.z);
+                if (distancePointToCamera < maxDist)
+                {
+                    float sqrDistance = (new Vector2(v.x, v.y) - screenPos).sqrMagnitude;
+                    if (sqrDistance < 25.0f)
+                    {
+                        closestHit = pWorld;
+                        maxDist = distancePointToCamera;
+                    }
+                }
+            }
+        }
+    }
+}

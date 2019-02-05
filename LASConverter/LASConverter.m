ad591a5c85a974824c259b1337c6e31fdf510fe1
amazingsmash <amazingsmash@gmail.com>
2019-02-05 10:57:55 +0000

    changes on converter

--- LASConverter/LASConverter.m
+++ LASConverter/LASConverter.m
@@ -38,27 +38,7 @@ classdef LASConverter
         
         end
         
-        function [pc1, pc2, pc3, pc4, pc5, pc6, pc7, pc8] = splitInOctree(pc)
-            x = pc(:,1);
-            y = pc(:,2);
-            z = pc(:,3);
-            cx = (max(x) + min(x)) / 2;
-            cy = (max(y) + min(y)) / 2;
-            cz = (max(z) + min(z)) / 2;
-            
-            px = x > cx;
-            py = y > cy;
-            pz = z > cz;
-            
-            pc1 = px & py & pz;
-            pc2 = px & py & ~pz;
-            pc3 = px & ~py & pz;
-            pc4 = px & ~py & ~pz;
-            pc5 = ~px & py & pz;
-            pc6 = ~px & py & ~pz;
-            pc7 = ~px & ~py & pz;
-            pc8 = ~px & ~py & ~pz;
-        end
+
 
         function LASFile2Bytes(filename, lasName, pointsPerFile)
             las = lasdata(filename);
@@ -100,28 +80,74 @@ classdef LASConverter
             end
         end
         
-        function LASData2Bytes_Octree(xyzClass, lasName, pointsPerFile, voxelName)
+        function LASData2Bytes_Octree(xyzClass, lasName, pointsPerFile, clearFolder)
             
-            if nargin == 3
-                LASConverter.resetFolder(lasName);
-                voxelName = "";
+            function [pc1, pc2, pc3, pc4, pc5, pc6, pc7, pc8] = splitInOctree(pc)
+                x = pc(:,1);
+                y = pc(:,2);
+                z = pc(:,3);
+                cx = (max(x) + min(x)) / 2;
+                cy = (max(y) + min(y)) / 2;
+                cz = (max(z) + min(z)) / 2;
+
+                px = x > cx;
+                py = y > cy;
+                pz = z > cz;
+
+                pc1 = px & py & pz;
+                pc2 = px & py & ~pz;
+                pc3 = px & ~py & pz;
+                pc4 = px & ~py & ~pz;
+                pc5 = ~px & py & pz;
+                pc6 = ~px & py & ~pz;
+                pc7 = ~px & ~py & pz;
+                pc8 = ~px & ~py & ~pz;
             end
             
-            if isempty(xyzClass)
-                return
-            end
-            
-            if length(xyzClass) < pointsPerFile
-                outFN = sprintf("%s/Points_Voxel_%s.bytes", lasName, voxelName);
-                LASConverter.save2DMatrixToBinary(outFN, xyzClass);
-            else
-                [pc1, pc2, pc3, pc4, pc5, pc6, pc7, pc8] = LASConverter.splitInOctree(xyzClass); 
-                voxels = {pc1, pc2, pc3, pc4, pc5, pc6, pc7, pc8};
-                
-                for i = 1:length(voxels)
-                    newVoxelName = sprintf("%s_%d", voxelName, i);
-                    LASConverter.LASData2Bytes_Octree(xyzClass(voxels{i}, :), lasName, pointsPerFile, newVoxelName)
+            function saveOctree(xyzClass, lasName, pointsPerFile, voxelName)
+                if isempty(xyzClass)
+                    return
                 end
+
+                if length(xyzClass) < pointsPerFile
+                    outFN = sprintf("%s/Points_Voxel_%s.bytes", lasName, voxelName);
+                    LASConverter.save2DMatrixToBinary(outFN, xyzClass);
+                else
+                    [pc1, pc2, pc3, pc4, pc5, pc6, pc7, pc8] = splitInOctree(xyzClass); 
+                    voxels = {pc1, pc2, pc3, pc4, pc5, pc6, pc7, pc8};
+
+                    for i = 1:length(voxels)
+                        newVoxelName = sprintf("%s_%d", voxelName, i);
+                        saveOctree(xyzClass(voxels{i}, :), lasName, pointsPerFile, newVoxelName)
+                    end
+
+                end
+            end
+            
+            if clearFolder
+                LASConverter.resetFolder(lasName);
+            end
+            saveOctree(xyzClass, lasName, pointsPerFile, "")
+        end
+        
+        function LASData2Bytes_Octree_MultiFile(filenames, lasName, pointsPerFile)
+            
+            
+            LASConverter.resetFolder(lasName);
+            pMin = nan;
+            for i = 1:length(filenames)
+                
+                fn = filenames{i};
+                load(fn);
+                
+                %Compute min of first
+                if isnan(pMin)
+                    pMin = [ min(points(:,1)), min(points(:,2)), min(points(:,3))];
+                end
+                
+                %Bringing cloud to zero
+                xyzClass = [points(:,1) - pMin(1), points(:,2) - pMin(2), points(:,3) - pMin(3), points(:,5)];
+                LASConverter.LASData2Bytes_Octree(xyzClass, lasName, pointsPerFile, false)
                 
             end
         end

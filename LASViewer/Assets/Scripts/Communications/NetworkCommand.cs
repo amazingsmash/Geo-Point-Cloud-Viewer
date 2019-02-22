528e2ef309af7ba116f7619844c3881c77c0113b
amazingsmash <amazingsmash@gmail.com>
2019-02-22 11:26:15 +0000

    lod fine tunning

---
+++ LASViewer/Assets/Scripts/Communications/NetworkCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime;
+
+[Serializable()]
+public class NetworkCommand {
+
+    public readonly string name;
+
+    public NetworkCommand(string name)
+    {
+        this.name = name;
+    }
+}
+
+public class SelectPointCmd : NetworkCommand
+{
+    public float x, y, z;
+
+    public SelectPointCmd(float x, float y, float z): base("SelectPoints") {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+}
+
---
+++ LASViewer/Assets/Scripts/Communications/RemoteServerPointCloudListener.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoteServerPointCloudListener : MonoBehaviour, IPointCloudListener, ITCPListener
+{
+    readonly TCPServer server;
+
+    public RemoteServerPointCloudListener()
+    {
+        server = new TCPServer(this);
+    }
+
+    public void OnMessageReceived(string msg)
+    {
+        Debug.Log(msg);
+    }
+
+    public void onPointSelected(Vector3 point)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = point;
+        float ballSize = 2.0f;
+        sphere.transform.localScale = new Vector3(ballSize, ballSize, ballSize);
+
+        SelectPointCmd selectPointCmd = new SelectPointCmd(0, 0, 0);
+
+        server.SendMessage("Point Selected: " + point.ToString());
+    }
+
+    public void OnStatusMessage(string msg)
+    {
+        Debug.Log(msg);
+    }
+}
